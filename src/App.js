ee93737e68d832246564a9e473809860b2e3668e
Cristina Altreche <c.altreche@gmail.com>
2020-07-31 16:43:16 -0400

    feature 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import React from 'react';
+import React from "react";
 
+//feature 1
 function App() {
   return (
     <div className="grid-container">
       <header>
         <a href="/">React Shopping Cart</a>
       </header>
-      <main>
-        Product List
-      </main>
-      <footer>
-        All rights reserved.
-      </footer>
+      <main>Product List</main>
+      <footer>All rights reserved.</footer>
     </div>
   );
 }
